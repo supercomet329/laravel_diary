d8fc849906a8084b7f6fba94f061abcbb7bb18a8
supercomet329 <supercomet329@gmail.com>
2019-06-01 18:14:44 +0900

    投稿画面に画像欄を追加

--- resources/views/diaries/create.blade.php
+++ resources/views/diaries/create.blade.php
@@ -15,7 +15,7 @@
                     @endforeach
                 </ul>
             @endif
-            <form action="{{ route('diary.create') }}" method="post">
+            <form action="{{ route('diary.create') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
                     <label for="title">タイトル</label>
@@ -24,6 +24,10 @@
                 <div class="form-group">
                     <label for="body">本文</label>
                     <textarea class="form-control" name="body" id="body">{{ old('body') }}</textarea>
+                </div>
+                <div class="form-group">
+                    <label for="image">画像</label>
+                    <input type="file" class="form-control" name="image" id="image" value="{{ old('image') }}">
                 </div>
                 <div class="text-right">
                     <button type="submit" class="btn btn-primary">投稿</button>

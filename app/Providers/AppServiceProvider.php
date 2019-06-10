40cfcc6b3b2e8e825d0123d9a100bbe8ddba9eac
supercomet329 <supercomet329@gmail.com>
2019-06-11 02:00:29 +0800

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
+        \URL::forceScheme('https');
 
         Schema::defaultStringLength(191);
     }

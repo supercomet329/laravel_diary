0897bcd56f64e5410cbfa17783480e2d5804be0d
supercomet329 <supercomet329@gmail.com>
2019-06-27 15:26:54 +0800

    バリデーション修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
-            'picture' => ['required', 'image', 'mimes:jpeg, png, jpg, gif', 'max:2048'],
+            'picture' => ['required', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
         ], [], [
             'name' => 'ユーザー名',
             'email' => 'メールアドレス',
